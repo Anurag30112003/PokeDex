09eb878bbde7856aca11a0ee3b7f7ac1a62a7dcd
Srianshu <email@example.com>
2022-08-21 00:11:56 +0530

    added

--- src/pages/api/[slug].ts
+++ src/pages/api/[slug].ts
@@ -17,17 +17,17 @@ export default async (req:NextApiRequest,res:NextApiResponse) => {
             
             const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon-shape/${slug}`)
             const data = await pokemon.json()
-                 
-        
-            const h =  await data.pokemon_species.forEach((r: { name: any; }) =>  {
-                r.name
-                return r;
-            }).then((r: { name: any; }) => {
-                return r.name;
-            }).catch((err: any) => {
-                return err;
+            const r = data.pokemon_species 
+            const k :any = []
+            r.forEach((element: any) => {
+             const  k =  element.name
+             console.log(k)
+             
             });
+            k
+            res.send(k)
             res.statusCode = 200
+            return;
     } catch (error) {
         res.statusCode = 500
         res.send(JSON.stringify({message:'error fetching pokemon'}))
