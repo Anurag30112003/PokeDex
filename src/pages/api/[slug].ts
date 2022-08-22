3f1fb80659de0f14249ba01432eec7b6536f0ea9
Anurag <anuragsharma011011@gmail.com>
2022-08-23 00:58:15 +0530

    Fetching sprites

--- src/pages/api/[slug].ts
+++ src/pages/api/[slug].ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
-const BASE_URL:string = "https://pokeapi.co/api/v2/pokemon-shape/";
+import getSprites from "../api/getSprites";
+const BASE_URL: string = "https://pokeapi.co/api/v2/pokemon-shape/";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const slug = req.query["slug"];
@@ -10,19 +11,21 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     return;
   }
   try {
-    const pokemon = await fetch(
-      `${BASE_URL}${slug}`
-    );
+    const pokemon = await fetch(`${BASE_URL}${slug}`);
     const data = await pokemon.json();
     const r = data.pokemon_species;
-    let h:string[] = [];
-    r.forEach((element: any) => {
-      const k = element.name;
-      h.push(k);
+    let h: string[] = [];
+    let t: string[] = [];
+
+    r.forEach(async (element: any) => {
+        const k = element.name;
+        const i = getSprites(k);
+        h.push(k);
+        t.push(await i);
     });
     res.statusCode = 200;
-    res.send(JSON.stringify(h));
-    return;
+    res.send(JSON.stringify({ pokemon: h, image: t }));
+
   } catch (error) {
     res.statusCode = 500;
     res.send(JSON.stringify({ message: "error fetching pokemon" }));
--- src/pages/api/getSprites.ts
+++ src/pages/api/getSprites.ts
@@ -1,6 +1,19 @@
 const BASE_URL :string = 'https://pokeapi.co/api/v2/pokemon/'
-const getSprites = (name :string) =>{
+export default async (name:string) => {
+    try {
+        const sprite = await fetch(
+            `${BASE_URL}${name}`
+        );
+        // if response returns a 404, return null
+        if (sprite.status === 404) {
+            return null;
+        }
+        const data = await sprite.json();
+        // console.log(data.sprites.front_default);
+        return( data.sprites.front_default);
+    } catch (error) {
+        console.log(error);
+    }
     
 }
 
-export default getSprites;
