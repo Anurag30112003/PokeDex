dbda39c071aa6a1d8f7849516be3cf2cb329623b
Anurag <anuragsharma011011@gmail.com>
2022-08-23 01:35:28 +0530

    Heading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,43 @@
-import type { NextPage } from 'next'
-import React from 'react'
-// import Head from 'next/head'
+import type { NextPage } from "next";
+import React from "react";
+import Head from "next/head";
 // import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   // slug as input and fetch data from /api/slug
-  const [slug] = React.useState('')
-  const [data , setData] = React.useState<any>()
-  const [isLoading , setIsLoading] = React.useState<boolean>(false)
+  const [slug] = React.useState("");
+  const [data, setData] = React.useState<any>();
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
   React.useEffect(() => {
-    setIsLoading(true)
+    setIsLoading(true);
     fetch(`/api/${slug}`)
-      .then(res => res.json())
-      .then(data => {
-        setData(data)
-        setIsLoading(false)
-      }).catch(err => {
-        console.log(err)
-        setIsLoading(false)
-      }
-    )
-  } , [])
-  if (isLoading) return <p>Loading...</p>
-
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
+  }, []);
+  if (isLoading) return <p>Loading...</p>;
 
   return (
-   <>
+    <>
+      <Head>
+        <title>Pokedex</title>
+        <meta name="description" content="pokemon api made easy" />
+      </Head>
       <h1>{data}</h1>
-    <form>
-      <input type="text" name="slug"  />
-      <button type="submit">Submit</button>
-    </form>
-   
-   </>
-  )
-}
+      <form>
+        <input type="text" name="slug" />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+};
 
-
-export default Home
+export default Home;
