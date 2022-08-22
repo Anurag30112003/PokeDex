64aa6810b98dab0c7d7e7616fbd8d4c66a6540db
Anurag <anuragsharma011011@gmail.com>
2022-08-22 23:07:49 +0530

    Slug successfull

--- src/pages/api/[slug].ts
+++ src/pages/api/[slug].ts
@@ -1,37 +1,30 @@
-import { NextApiRequest,NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon-shape/";
 
-export default async (req:NextApiRequest,res:NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const slug = req.query["slug"];
 
-    const slug = req.query['slug']
-
-    if (!slug || typeof slug !== 'string'){
-
-        res.statusCode = 404
-        res.send(JSON.stringify({message:'please provide a slug'}))
-        return;
-    } 
-    // fetch pokemon api with slug as id
-    // return pokemon data
-
-    try {
-            
-            const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon-shape/${slug}`)
-            const data = await pokemon.json()
-            const r = data.pokemon_species 
-            const k :any = []
-            r.forEach((element: any) => {
-             const  k =  element.name
-             console.log(k)
-             
-            });
-            k
-            res.send(k)
-            res.statusCode = 200
-            return;
-    } catch (error) {
-        res.statusCode = 500
-        res.send(JSON.stringify({message:'error fetching pokemon'}))
-    }
-}
-
-
+  if (!slug || typeof slug !== "string") {
+    res.statusCode = 404;
+    res.send(JSON.stringify({ message: "please provide a slug" }));
+    return;
+  }
+  try {
+    const pokemon = await fetch(
+      `${BASE_URL}${slug}`
+    );
+    const data = await pokemon.json();
+    const r = data.pokemon_species;
+    const h :any = []
+    r.forEach((element: any) => {
+      const k = element.name;
+      h.push(k);
+    });
+    res.statusCode = 200;
+    res.send(JSON.stringify(h));
+    return;
+  } catch (error) {
+    res.statusCode = 500;
+    res.send(JSON.stringify({ message: "error fetching pokemon" }));
+  }
+};
